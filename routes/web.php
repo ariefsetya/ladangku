7345ada7940991054a446b77643263bef1b31065
Arief Setya <ariefsetya@live.com>
2017-01-01 04:51:14 +0700

    update farmer and models

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/farmer',['as'=>'allFarmer','uses'=>'AdminController@farmer_all']);
+Route::get('admin/farmer/add',['as'=>'addFarmer','uses'=>'AdminController@farmer_add']);
+Route::post('admin/farmer/save',['as'=>'saveFarmer','uses'=>'AdminController@farmer_save']);
+Route::get('admin/farmer/edit/{id}',['as'=>'editFarmer','uses'=>'AdminController@farmer_edit']);
+Route::post('admin/farmer/update',['as'=>'updateFarmer','uses'=>'AdminController@farmer_update']);
+Route::get('admin/farmer/delete/{id}',['as'=>'deleteFarmer','uses'=>'AdminController@farmer_delete']);
