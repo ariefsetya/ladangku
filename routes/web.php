bb372adffc2c75ff2298b19be051e8c9f4c71227
Achmad fauzi <Achmad fauzi>
2017-01-15 21:37:51 +0700

    up landing page

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
 
 Route::get('admin/farmer',['as'=>'allFarmer','uses'=>'AdminController@farmer_all']);
